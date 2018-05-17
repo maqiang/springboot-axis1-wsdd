1fb96ee33f2908484522764a3826da909141d84c
Pony <ma.qiang.jx@gmail.com>
2018-05-17 09:33:45 +0800

    本地提交

---
+++ springboot-axis1-wsdd/src/main/java/org/github/axis1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.github.axis1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootAxis1WsddApplication.class);
+	}
+
+}
---
+++ springboot-axis1-wsdd/src/main/java/org/github/axis1/SpringbootAxis1WsddApplication.java
@@ -0,0 +1,12 @@
+package org.github.axis1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAxis1WsddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAxis1WsddApplication.class, args);
+	}
+}
